65336ffbb186807b26332b5f4cf72067ef9ce99d
HOJcracker <31023698+hojcracker@users.noreply.github.com>
2019-09-14 14:11:01 -0300

    Version 1.0
    Works fine with very few and rare bugs

---
+++ Slide down the ramp/Slide_down/Slide_down/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Slide_down
+{
+    partial class Ball_sliding
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ball_sliding));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bunifuThinButton21 = new ns1.BunifuThinButton2();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.angle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gravity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.length = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.variables = new System.Windows.Forms.ListBox();
+            this.ball = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.bunifuThinButton21);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(423, 61);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseMove);
+            // 
+            // bunifuThinButton21
+            // 
+            this.bunifuThinButton21.ActiveBorderThickness = 1;
+            this.bunifuThinButton21.ActiveCornerRadius = 20;
+            this.bunifuThinButton21.ActiveFillColor = System.Drawing.Color.Red;
+            this.bunifuThinButton21.ActiveForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.Red;
+            this.bunifuThinButton21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.bunifuThinButton21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuThinButton21.BackgroundImage")));
+            this.bunifuThinButton21.ButtonText = "X";
+            this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuThinButton21.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuThinButton21.ForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuThinButton21.IdleBorderThickness = 1;
+            this.bunifuThinButton21.IdleCornerRadius = 20;
+            this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.bunifuThinButton21.IdleForecolor = System.Drawing.Color.Red;
+            this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.Red;
+            this.bunifuThinButton21.Location = new System.Drawing.Point(376, 1);
+            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(5);
+            this.bunifuThinButton21.Name = "bunifuThinButton21";
+            this.bunifuThinButton21.Size = new System.Drawing.Size(47, 47);
+            this.bunifuThinButton21.TabIndex = 1;
+            this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuThinButton21.Click += new System.EventHandler(this.BunifuThinButton21_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(5, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ball sliding down";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(272, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "V1.0";
+            // 
+            // angle
+            // 
+            this.angle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.angle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.angle.ForeColor = System.Drawing.Color.White;
+            this.angle.Location = new System.Drawing.Point(241, 67);
+            this.angle.Multiline = true;
+            this.angle.Name = "angle";
+            this.angle.Size = new System.Drawing.Size(157, 26);
+            this.angle.TabIndex = 1;
+            this.angle.TextChanged += new System.EventHandler(this.Angle_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(7, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Angle of the slope: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(7, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Gravity force";
+            // 
+            // gravity
+            // 
+            this.gravity.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.gravity.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gravity.ForeColor = System.Drawing.Color.White;
+            this.gravity.Location = new System.Drawing.Point(160, 104);
+            this.gravity.Multiline = true;
+            this.gravity.Name = "gravity";
+            this.gravity.Size = new System.Drawing.Size(157, 26);
+            this.gravity.TabIndex = 4;
+            this.gravity.TextChanged += new System.EventHandler(this.Gravity_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(7, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(188, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Length in meters";
+            // 
+            // length
+            // 
+            this.length.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.length.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.length.ForeColor = System.Drawing.Color.White;
+            this.length.Location = new System.Drawing.Point(201, 136);
+            this.length.Multiline = true;
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(157, 26);
+            this.length.TabIndex = 6;
+            this.length.TextChanged += new System.EventHandler(this.Length_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(7, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(184, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Time in seconds";
+            // 
+            // time
+            // 
+            this.time.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.time.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.time.ForeColor = System.Drawing.Color.White;
+            this.time.Location = new System.Drawing.Point(201, 168);
+            this.time.Multiline = true;
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(157, 26);
+            this.time.TabIndex = 8;
+            this.time.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(7, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 48);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // variables
+            // 
+            this.variables.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.variables.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.variables.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.variables.ForeColor = System.Drawing.Color.White;
+            this.variables.FormattingEnabled = true;
+            this.variables.ItemHeight = 25;
+            this.variables.Items.AddRange(new object[] {
+            "Angle",
+            "Gravity",
+            "Length",
+            "Time"});
+            this.variables.Location = new System.Drawing.Point(174, 212);
+            this.variables.Name = "variables";
+            this.variables.Size = new System.Drawing.Size(104, 125);
+            this.variables.TabIndex = 11;
+            // 
+            // ball
+            // 
+            this.ball.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ball.BackgroundImage")));
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ball.Location = new System.Drawing.Point(12, 345);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(51, 53);
+            this.ball.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(29)))), ((int)(((byte)(31)))));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(7, 435);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(412, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "The angle will be outputted as its sine";
+            // 
+            // Ball_sliding
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(55)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(422, 469);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.variables);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.length);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gravity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.angle);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Ball_sliding";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private ns1.BunifuThinButton2 bunifuThinButton21;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox angle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox gravity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox length;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox time;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox variables;
+        private System.Windows.Forms.Panel ball;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Slide down the ramp/Slide_down/Slide_down/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slide_down
+{
+    public partial class Ball_sliding : Form
+    {
+        public Ball_sliding()
+        {
+            InitializeComponent();
+        }
+
+        private void BunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            base.Close();
+        }
+
+        private void Angle_TextChanged(object sender, EventArgs e)
+        {
+            if (angle.Text == "")
+            {
+                angle.Text = "0";
+            }
+            try
+            {
+                double anglee = Convert.ToDouble(angle.Text);
+            }
+            catch (Exception ex)
+            {
+                angle.Text = "0";
+                MessageBox.Show("Invalid input");
+            }
+        }
+
+        private void Gravity_TextChanged(object sender, EventArgs e)
+        {
+            if (gravity.Text == "")
+            {
+                gravity.Text = "0";
+            }
+            try
+            {
+                double gravityy = Convert.ToDouble(gravity.Text);
+            }
+            catch (Exception ex)
+            {
+                gravity.Text = "0";
+                MessageBox.Show("Invalid input");
+            }
+        }
+
+        private void Length_TextChanged(object sender, EventArgs e)
+        {
+            if (length.Text == "")
+            {
+                length.Text = "0";
+            }
+            try
+            {
+                double lengthh = Convert.ToDouble(length.Text);
+            }
+            catch (Exception ex)
+            {
+                length.Text = "0";
+                MessageBox.Show("Invalid input");
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (time.Text == "")
+            {
+                time.Text = "0";
+            }
+            try
+            {
+                double timee = Convert.ToDouble(time.Text);
+            }
+            catch (Exception ex)
+            {
+                time.Text = "0";
+                MessageBox.Show("Invalid input");
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 12; i < 350; i++)
+            {
+                ball.Location = new Point(i,ball.Location.Y);
+            }
+            ball.Location = new Point(12, ball.Location.Y);
+            if (variables.SelectedIndex == 0)
+            {
+                double gravity_force = Convert.ToDouble(gravity.Text);
+                double slope_length = Convert.ToDouble(length.Text);
+                double seconds = Convert.ToDouble(time.Text);
+                double slope_angle = (2 * slope_length) / (gravity_force * Math.Pow(seconds, 2));
+                angle.Text = Convert.ToString(slope_length);
+            }
+            if (variables.SelectedIndex == 1)
+            {
+                double slope_angle = Convert.ToDouble(angle.Text);
+                double slope_length = Convert.ToDouble(length.Text);
+                double seconds = Convert.ToDouble(time.Text);
+                double gravity_force = (2 * slope_length) / (Math.Sin(slope_angle) * Math.Pow(seconds, 2));
+                gravity.Text = Convert.ToString(gravity_force);
+            }
+            if (variables.SelectedIndex == 2)
+            {
+                double slope_angle = Convert.ToDouble(angle.Text);
+                double gravity_force = Convert.ToDouble(gravity.Text);
+                double acceleration = Math.Sin(slope_angle) * gravity_force;
+                double seconds = Convert.ToDouble(time.Text);
+                double slope_length = acceleration * Math.Pow(seconds, 2);
+                length.Text = Convert.ToString(slope_length);
+            }
+            if (variables.SelectedIndex == 3)
+            {
+                double slope_angle = Convert.ToDouble(angle.Text);
+                double gravity_force = Convert.ToDouble(gravity.Text);
+                double slope_length = Convert.ToDouble(length.Text);
+                double acceleration = Math.Sin(slope_angle) * gravity_force;
+                double seconds = Math.Sqrt((2 * slope_length) / acceleration);
+                time.Text = Convert.ToString(seconds);
+            }
+        }
+
+        private void Panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+        Point lastPoint;
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+    }
+}
